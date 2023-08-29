b79a56b7fa253ba7979d51f8d830669dc9232879
ClausKlein <claus.klein@arcormail.de>
2023-08-29 11:02:57 +0200

    Use cpmfindpackage if possible
    Use current version of ftxui package with UNITY_BUILD fix.

--- Dependencies.cmake
+++ Dependencies.cmake
@@ -10,31 +10,47 @@ function(myproject_setup_dependencies)
 
   if(NOT TARGET fmtlib::fmtlib)
     cpmaddpackage("gh:fmtlib/fmt#9.1.0")
+    # TODO(CK) this does not work?
+    # cpmfindpackage(
+    #   NAME
+    #   fmtlib
+    #   GIT_TAG
+    #   9.1.0
+    #   VERSION
+    #   9.1.0
+    #   GITHUB_REPOSITORY
+    #   "fmtlib/fmt")
   endif()
 
-  if(NOT TARGET spdlog::spdlog)
-    cpmaddpackage(
-      NAME
-      spdlog
-      VERSION
-      1.11.0
-      GITHUB_REPOSITORY
-      "gabime/spdlog"
-      OPTIONS
-      "SPDLOG_FMT_EXTERNAL ON")
-  endif()
+  cpmfindpackage(
+    NAME
+    spdlog
+    VERSION
+    1.12.0
+    GITHUB_REPOSITORY
+    "gabime/spdlog"
+    OPTIONS
+    "SPDLOG_FMT_EXTERNAL ON")
 
   if(NOT TARGET Catch2::Catch2WithMain)
-    cpmaddpackage("gh:catchorg/Catch2@3.3.2")
+    cpmaddpackage("gh:catchorg/Catch2@3.4.0")
   endif()
 
   if(NOT TARGET CLI11::CLI11)
     cpmaddpackage("gh:CLIUtils/CLI11@2.3.2")
   endif()
 
-  if(NOT TARGET ftxui::screen)
-    cpmaddpackage("gh:ArthurSonzogni/FTXUI#e23dbc7473654024852ede60e2121276c5aab660")
-  endif()
+  cpmfindpackage(
+    NAME
+    FTXUI
+    VERSION
+    5.0.0
+    GIT_TAG
+    dd6a5d371fd7a3e2937bb579955003c54b727233
+    GITHUB_REPOSITORY
+    "ArthurSonzogni/FTXUI"
+    OPTIONS
+    "FTXUI_BUILD_EXAMPLES OFF; FTXUI_BUILD_DOCS OFF")
 
   if(NOT TARGET tools::tools)
     cpmaddpackage("gh:lefticus/tools#update_build_system")
